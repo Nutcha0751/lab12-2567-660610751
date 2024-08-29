0dc9f8f147c9ab42b700ccf81e40ba785307650a
Nutcha0751 <nutcha_kham@cmu.ac.th>
2024-08-29 15:15:32 +0700

    readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
           <Controller />
         </LangContext.Provider>
       </ThemeContext.Provider>
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="660610999" />
+      <Footer year="2024" fullName="Nutcha Khampoung" studentId="660610751" />
     </div>
   );
 }
